36e39a64f3a74c6ebd53bc545c30709ea32d7843
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-11 12:39:19 +0100

    commit

--- hotel-management-system/src/ui/Button.jsx
+++ hotel-management-system/src/ui/Button.jsx
@@ -50,12 +50,15 @@ const variations = {
 
   const Button = styled.button`
   font-size: 1.5rem;
-  padding: 1.4rem 1.6rem;
+  padding: 1.2rem 1.6rem;
   font-weight: 500;
   border: none;
   border-radius: 7px;
   background-color: #9c0795;
   color: #fff;
+  &:hover{
+    background-color: var(--color-brand-700);
+  }
   `;
 
   export default Button;  
