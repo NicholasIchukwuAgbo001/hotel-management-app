8ff4ae57bf25f1383b13eb9bec81b0689df32201
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-11 05:02:10 +0100

    commit

--- hotel-management-system/src/App.jsx
+++ hotel-management-system/src/App.jsx
@@ -6,10 +6,21 @@ const H1 = styled.h1`
   margin-bottom: 1rem;
   `;
 
+  const Button = styled.button`
+  font-size: 1.5rem;
+  padding: 1.4rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: #9c0795;
+  color: #fff;
+  `;
+
 const App = () => {
   return (
     <div>
      <H1> Hello World </H1>
+     <Button> Click me </Button>
     </div>
   )
 }
