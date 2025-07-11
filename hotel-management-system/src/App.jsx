d05fcd5bef39436e54467fd39fa760d05c3f9a2b
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-11 15:50:21 +0100

    commit

--- hotel-management-system/src/App.jsx
+++ hotel-management-system/src/App.jsx
@@ -3,6 +3,7 @@ import Button from './ui/Button';
 import GlobalStyles from './styles/GlobalStyles'; 
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyleApp = styled.main`
   background-color: orangered;
@@ -13,8 +14,8 @@ const App = () => {
   return (
     <>
       <GlobalStyles />
-
       <StyleApp>
+        <Row />
         <Heading type='h1'>Hello World</Heading>
 
         <Heading type='h2'>Hello World</Heading>
@@ -24,7 +25,7 @@ const App = () => {
 
         <Heading type='h3'>Hello World</Heading>
         
-        <Input type="text" placeholder="Enter your name" />
+        <Input type="number" placeholder="phone number" />
       </StyleApp>
     </>
   );
