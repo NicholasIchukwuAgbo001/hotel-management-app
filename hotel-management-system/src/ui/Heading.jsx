7702db7e2929d6419d7b6239eb2203f0dbafad5c
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-11 13:31:30 +0100

    commit

--- hotel-management-system/src/ui/Heading.jsx
+++ hotel-management-system/src/ui/Heading.jsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
+const fontSizes = {
+  h1: "4rem",
+  h2: "3rem",
+  h3: "2.2rem",
+};
+
+const fontWeights = {
+  h1: 600,
+  h2: 500,
+  h3: 400,
+};
+
 const Heading = styled.h1`
-  font-size: 4rem;
-  font-weight: 600;
-  background-color: yellow;
+  font-size: ${(props) => fontSizes[props.type] || "2rem"};
+  font-weight: ${(props) => fontWeights[props.type] || 400};
 `;
 
 export default Heading;
