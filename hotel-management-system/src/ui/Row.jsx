f7a0f71e842e57dc15e97cb24f407dcd18c2fda3
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-12 17:57:18 +0100

    commit

--- hotel-management-system/src/ui/Row.jsx
+++ hotel-management-system/src/ui/Row.jsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 const Row = styled.div`
   display: flex;
-
+  
   ${props => props.type === 'horizontal' && css`
     justify-content: space-between;
     align-items: center;
