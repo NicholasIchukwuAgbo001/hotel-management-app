8560c0ebe0f1575f2042c62ae60af77e87224ab7
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-18 15:30:11 +0100

    commit

--- hotel-management-system/src/App.jsx
+++ hotel-management-system/src/App.jsx
@@ -7,15 +7,11 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
 import Settings from "./pages/Settings";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
-import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeProvider } from "./context/DarkModeContext";
 
 const queryClient = new QueryClient({
@@ -36,25 +32,16 @@ function App() {
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
+            <Route path="/" element={<AppLayout />}>
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
               <Route path="bookings/:bookingId" element={<Booking />} />
               <Route path="checkin/:bookingId" element={<Checkin />} />
               <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
               <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
             </Route>
 
-            <Route path="login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
